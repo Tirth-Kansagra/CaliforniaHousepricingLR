c4f2f9129c48ac9725d0de1fb2793305c052ac17
Tirth Kansagra <202303055@dau.ac.in>
2026-07-17 16:06:35 +0530

    Commit For WebApp

--- app.py
+++ app.py
@@ -6,14 +6,35 @@ import pandas as pd
 app=Flask(__name__)
 ##Load the model
 regmodel=pickle.load(open('regmodel.pkl','rb'))
+scalar=pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
-    data = request.get(data)
-    data_df = pd.DataFrame([data])
-    prediction = regmodel.predict(data_df)
-    return jsonify({'prediction': prediction[0]})
+    data = request.get_json()
+
+    if not isinstance(data, dict):
+        return jsonify({"error": "Invalid JSON payload"}), 400
+
+    features = np.array([[
+        data["MedInc"],
+        data["HouseAge"],
+        data["AveRooms"],
+        data["AveBedrms"],
+        data["Population"],
+        data["AveOccup"],
+        data["Latitude"],
+        data["Longitude"]
+    ]])
+
+    print(features)
+    new_data = scalar.transform(features)
+    output = regmodel.predict(new_data)
+    print(output[0])
+    return jsonify({"prediction": float(output[0])})
+
+if __name__ == "__main__":
+    app.run(debug=True)
